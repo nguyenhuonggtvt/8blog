94b431a60882b39062cabd023f32ec1daea85dd5
Huong Nguyen <nguyenvanhuonghg92@gmail.com>
2024-10-19 05:58:27 +0900

    add tailwind css

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
     <div>
         <h1>Welcome to the INDEX page</h1>
         <AppAlert>This is an auto-imported component</AppAlert>
+        <button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Hover me</button>
     </div>
 </template>
