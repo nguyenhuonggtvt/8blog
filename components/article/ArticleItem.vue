f84dea84480a508907032297e3c12ef160f199c2
Huong Nguyen <nguyenvanhuonghg92@gmail.com>
2024-10-20 08:25:44 +0900

    pinia

---
+++ components/article/ArticleItem.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="group cursor-pointer">
+        <div class=" overflow-hidden rounded-md bg-gray-100 transition-all hover:scale-105 dark:bg-gray-800">
+            <a class="relative block aspect-square" href="/post/14-architectural-design-ideas-for-spacious-interior">
+                <img class="aspect-square object-cover" :src="article.image" alt="">
+            </a>
+        </div>
+        <div class="">
+            <div>
+                <div class="flex gap-3">
+                    <a href="/category/design">
+                        <span class="inline-block text-xs font-medium tracking-wider uppercase mt-5 text-blue-600">{{ article.categories[0] }}</span>
+                    </a>
+                    <a href="/category/lifestyle">
+                        <span class="inline-block text-xs font-medium tracking-wider uppercase mt-5 text-purple-600">{{ article.categories[1] }}</span>
+                    </a>
+                </div>
+                <h2 class="text-lg font-semibold leading-snug tracking-tight mt-2 dark:text-white">
+                    <a href="/post/14-architectural-design-ideas-for-spacious-interior">
+                        <span class="bg-gradient-to-r from-green-200 to-green-100 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px] dark:from-purple-800 dark:to-purple-900">{{ article.title }}</span>
+                    </a>
+                </h2>
+                <div class="">
+                    <p class="mt-2 line-clamp-3 text-sm text-gray-500 dark:text-gray-400">
+                        <a href="/post/14-architectural-design-ideas-for-spacious-interior">{{ article.description }}</a>
+                    </p>
+                </div>
+                <div class="mt-3 flex items-center space-x-3 text-gray-500 dark:text-gray-400">
+                    <a href="/author/mario-sanchez">
+                        <div class="flex items-center gap-3">
+                            <div class="relative h-5 w-5 flex-shrink-0">
+                                <img :src="article.author.profile_img" alt="">
+                            </div>
+                            <span class="truncate text-sm">{{ article.author.name }}</span>
+                        </div>
+                    </a>
+                    <span class="text-xs text-gray-300 dark:text-gray-600">•</span>
+                    <time class="truncate text-sm" datetime="2022-10-21T06:05:00.000Z">{{ article.publish_date }}</time>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+	defineProps({
+        article: Object,
+        default: () => ({})
+    });
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,87 @@
 <template>
-    <div>
-        <h1>Welcome to the INDEX page</h1>
-        <AppAlert>This is an auto-imported component</AppAlert>
-        <button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Hover me</button>
+    <div id="index_page">
+        <!-- Login User -->
+        <div>
+            <p>Name: {{ userStore.name }}</p>
+            <p>Logged In: {{ userStore.isLoggedIn }}</p>
+            <button @click="userStore.login">Login</button>
+            <button @click="userStore.logout">Logout</button>
+        </div>
+
+        <!-- List Item -->
+        <div class="container mx-auto flex justify-between items-center p-4">
+            <div class="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
+                <!-- Item -->
+                <ArticleItem v-for="(article, key) in articles" :key="key" :article="article"/>
+            </div>
+        </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+    import { useUserStore } from '@/stores/user'
+    const userStore = useUserStore()
+
+	const articles = [
+        {
+            title: "14 Architectural Design Ideas for a Spacious Interior",
+            description: 'It is a cliche philosophical question, but it touches on something fundamental about how humans relate to the world around them.',
+            image: 'https://static.vecteezy.com/system/resources/thumbnails/034/979/467/small_2x/ai-generated-beautiful-landscape-with-green-meadows-and-blue-sky-with-clouds-photo.jpg',
+            author: {
+                name: 'Mario Sanchez',
+                profile_img: 'https://as1.ftcdn.net/v2/jpg/05/52/03/68/1000_F_552036876_1tlOoZo90nkcBnotqdoVgB8FBrSHwQBN.jpg',
+            },
+            categories: ['Design', 'Lifestyle'],
+            publish_date: 'October 21, 2022',
+        },
+        {
+            title: "Nguyen Van Huong",
+            description: 'Noi dung mo ta.',
+            image: 'https://as1.ftcdn.net/v2/jpg/05/52/03/68/1000_F_552036876_1tlOoZo90nkcBnotqdoVgB8FBrSHwQBN.jpg',
+            author: {
+                name: 'Nguyen Huong',
+                profile_img: 'https://as1.ftcdn.net/v2/jpg/05/52/03/68/1000_F_552036876_1tlOoZo90nkcBnotqdoVgB8FBrSHwQBN.jpg',
+            },
+            categories: ['Thiet ke', 'Japanese'],
+            publish_date: 'October 20, 2024',
+        },
+        {
+            title: "14 Architectural Design Ideas for a Spacious Interior",
+            description: 'It is a cliche philosophical question, but it touches on something fundamental about how humans relate to the world around them.',
+            image: 'https://static.vecteezy.com/system/resources/thumbnails/034/979/467/small_2x/ai-generated-beautiful-landscape-with-green-meadows-and-blue-sky-with-clouds-photo.jpg',
+            author: {
+                name: 'Mario Sanchez',
+                profile_img: 'https://as1.ftcdn.net/v2/jpg/05/52/03/68/1000_F_552036876_1tlOoZo90nkcBnotqdoVgB8FBrSHwQBN.jpg',
+            },
+            categories: ['Design', 'Lifestyle'],
+            publish_date: 'October 21, 2022',
+        },
+        {
+            title: "14 Architectural Design Ideas for a Spacious Interior",
+            description: 'It is a cliche philosophical question, but it touches on something fundamental about how humans relate to the world around them.',
+            image: 'https://static.vecteezy.com/system/resources/thumbnails/034/979/467/small_2x/ai-generated-beautiful-landscape-with-green-meadows-and-blue-sky-with-clouds-photo.jpg',
+            author: {
+                name: 'Mario Sanchez',
+                profile_img: 'https://as1.ftcdn.net/v2/jpg/05/52/03/68/1000_F_552036876_1tlOoZo90nkcBnotqdoVgB8FBrSHwQBN.jpg',
+            },
+            categories: ['Design', 'Lifestyle'],
+            publish_date: 'October 21, 2022',
+        },
+        {
+            title: "14 Architectural Design Ideas for a Spacious Interior",
+            description: 'It is a cliche philosophical question, but it touches on something fundamental about how humans relate to the world around them.',
+            image: 'https://static.vecteezy.com/system/resources/thumbnails/034/979/467/small_2x/ai-generated-beautiful-landscape-with-green-meadows-and-blue-sky-with-clouds-photo.jpg',
+            author: {
+                name: 'Mario Sanchez',
+                profile_img: 'https://as1.ftcdn.net/v2/jpg/05/52/03/68/1000_F_552036876_1tlOoZo90nkcBnotqdoVgB8FBrSHwQBN.jpg',
+            },
+            categories: ['Design', 'Lifestyle'],
+            publish_date: 'October 21, 2022',
+        },
+    ];
+
+</script>
+
+<style>
+
+</style>
